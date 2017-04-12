81e74285359f9316707779ab63195703281e439b
theClubhou.se <heythere@theclubhou.se>
2017-04-12 15:59:24 -0400

    postdata

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -11,7 +11,9 @@ class ArticlesController extends Controller
     //List of Articles
     public function index()
     {
+      $articles = Article::all();
 
+      return Response::json($articles);
     }
     //Stores our Articles
     public function store(Request $request)
@@ -29,20 +31,44 @@ class ArticlesController extends Controller
 
       return Response::json(['success' => 'Congrats! You did it.']);
     }
-    //Update our Articles
+    //Update our Articles.
     public function update($id, Request $request)
     {
+      //Finds a specific Article based on ID.
+      $article = Article::find($id);
 
+      //Saves the Title
+      $article->title = $request->input('title');
+      //Saves the Body
+      $article->body = $request->input('body');
+
+      //Moves the Image to the server and saves the Image URL to the DB.
+      $image = $request->file('image');
+      $imageName = $image->getClientOriginalName();
+      $image->move("storage/", $imageName);
+      $article->image = $request->root(). "/storage/".$imageName;
+
+      //Commits the Saves to the DataBase.
+      $article->save();
+
+      //Sends a message back to the Front-End.
+      return Response::json(["success" => "Article Updated"]);
     }
     //Show single Article
     public function show($id)
     {
+      $article = Article::find($id);
 
+      return Response::json($article);
     }
     //Delete a single Articles
     public function destroy($id)
     {
+      $article = Article::find($id);
 
+      $article->delete();
+
+      return Response::json(['success' => 'Deleted Article.']);
     }
 
 }
